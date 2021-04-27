2e7607b77a4fbe491c056a7fb2dec311453f4ced
MikeDel88 <delamarre.michael@gmail.com>
2021-04-27 16:12:08 +0200

    test

--- src/main/java/com/example/lessgo/controllers/MyRestController.java
+++ src/main/java/com/example/lessgo/controllers/MyRestController.java
@@ -2,6 +2,7 @@ package com.example.lessgo.controllers;
 
 import com.example.lessgo.model.UserBean;
 import com.example.lessgo.model.UserDao;
+import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,15 +15,17 @@ public class MyRestController {
     @Autowired
     private UserDao userDao;
 
-
-
+    private Gson json;
 
     // Doit retourner la liste la liste des positions des utilisateurs équipe et autres équipe
     @GetMapping("/getPositions")
     public String getPositions(){
 
             ArrayList<UserBean> users = (ArrayList<UserBean>) userDao.findAll();
-            return "test";
+            if(users.isEmpty()){
+                return "test";
+            }
+            return json.toJson(users);
     }
 
 }
