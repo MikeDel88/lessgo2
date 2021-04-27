b822bde03e82f1c3ff2a2cae5e7ec5be74bc3c60
MikeDel88 <delamarre.michael@gmail.com>
2021-04-27 10:25:23 +0200

    Initial commit

---
+++ src/main/java/com/example/lessgo/LessgoApplication.java
@@ -0,0 +1,13 @@
+package com.example.lessgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LessgoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LessgoApplication.class, args);
+    }
+        //
+}
---
+++ src/test/java/com/example/lessgo/LessgoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lessgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LessgoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
