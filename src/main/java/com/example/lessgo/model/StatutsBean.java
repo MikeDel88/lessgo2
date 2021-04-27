7aaabea685d7ab73c0ff5b2397efea2c4b0e9024
nathanmanigaut <nathan.manigaut@gmail.com>
2021-04-27 14:53:20 +0200

    Getter et setter

--- src/main/java/com/example/lessgo/model/StatutsBean.java
+++ src/main/java/com/example/lessgo/model/StatutsBean.java
@@ -9,4 +9,20 @@ public class StatutsBean {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long statuts_id;
     private String nom;
+
+    public long getStatuts_id() {
+        return statuts_id;
+    }
+
+    public void setStatuts_id(long statuts_id) {
+        this.statuts_id = statuts_id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
 }
--- src/main/java/com/example/lessgo/model/UserBean.java
+++ src/main/java/com/example/lessgo/model/UserBean.java
@@ -13,4 +13,60 @@ public class UserBean {
     private Double lat;
     private Double lon;
     private long statuts_id;
+
+    public long getUsers_id() {
+        return users_id;
+    }
+
+    public void setUsers_id(long users_id) {
+        this.users_id = users_id;
+    }
+
+    public String getPseudo() {
+        return pseudo;
+    }
+
+    public void setPseudo(String pseudo) {
+        this.pseudo = pseudo;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Boolean getOther_team() {
+        return other_team;
+    }
+
+    public void setOther_team(Boolean other_team) {
+        this.other_team = other_team;
+    }
+
+    public Double getLat() {
+        return lat;
+    }
+
+    public void setLat(Double lat) {
+        this.lat = lat;
+    }
+
+    public Double getLon() {
+        return lon;
+    }
+
+    public void setLon(Double lon) {
+        this.lon = lon;
+    }
+
+    public long getStatuts_id() {
+        return statuts_id;
+    }
+
+    public void setStatuts_id(long statuts_id) {
+        this.statuts_id = statuts_id;
+    }
 }
